05b1ede66180f9a5c0cd7251e98fbf23b1e08762
Dzmitry <dima.semnovich.01@ickoud.com>
2021-06-28 17:56:36 +0300

    feat: implemented fetch and sore data

---
+++ Profile_test/Profile_test/Profile/FetchData.swift
@@ -0,0 +1,55 @@
+//
+//  FetchData.swift
+//  Profile_test
+//
+//  Created by Dzmitry Semenovich on 28.06.21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class FetchData {
+    let appDelegate: AppDelegate
+    let context: NSManagedObjectContext
+    let entity: NSEntityDescription?
+    let request: NSFetchRequest<NSFetchRequestResult>
+    
+    init() {
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        entity = NSEntityDescription.entity(forEntityName: "Profile", in: context) ?? nil
+        request = NSFetchRequest<NSFetchRequestResult>(entityName: "Profile")
+    }
+    
+    func fetchData() -> ProfileData? {
+        do {
+            let result = try context.fetch(request)
+            
+            for data in result as! [NSManagedObject] {
+                
+                let name = data.value(forKey: "name") as! String
+                let dbirth = data.value(forKey: "dbirth") as! String
+                let height = data.value(forKey: "height") as! String
+                let bio = data.value(forKey: "bio") as? String
+                let image = data.value(forKey: "image") as! UIImage
+                
+                return ProfileData(name: name, dbirth: dbirth, height: height, bio: bio, image: image)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+}
+
+struct ProfileData {
+    var name: String
+    var dbirth: String
+    var height: String
+    var bio: String?
+    let image: UIImage
+}
+
+
--- Profile_test/Profile_test/Profile/ProfileViewController.swift
+++ Profile_test/Profile_test/Profile/ProfileViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let fetch = FetchData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
     }
     
     func showAlertWith(title: String, message: String){
